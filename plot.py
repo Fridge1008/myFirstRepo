c7872e988608aa6abad0f932905e5e9cfcf9412a
Fridge1008 <james.palmer1008@gmail.com>
2023-03-05 17:55:32 +1100

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([1, 8])
-ypoints = np.array([3, 10])
+xpoints = np.array([1, 10])
+ypoints = np.array([3, 9])
 
 plt.plot(xpoints, ypoints)
 plt.show()
